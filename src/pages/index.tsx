dc7984f35e42232b24a61ffe3b44dabd70e98cc4
Tyler Hall <tacolegs2004@Tylers-MBP.lan>
2023-06-05 10:52:06 -0500

    init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { api } from "~/utils/api";
+
+const Home: NextPage = () => {
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
+
+  return (
+    <>
+      <main className="h-screen w-screen bg-purple-500">
+        <h1>hello</h1>
+        <div>
+          {!isSignedIn && (
+            <div className="w-23 flex justify-center rounded-md bg-red-500">
+              <SignInButton />
+            </div>
+          )}
+        </div>
+        <div>
+          {!!isSignedIn && (
+            <div className="w-23 flex justify-center rounded-md bg-red-500">
+              <SignOutButton />
+            </div>
+          )}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
