bd3af7707685830a9b1a0cad7cc4fb8054ccc595
Tyler Hall <tacolegs2004@Tylers-MBP.lan>
2023-06-06 09:44:29 -0500

    just more organizing and deleting unused stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
 import Navbar from "~/components/Navbar";
-import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const { isLoaded: userLoaded, isSignedIn } = useUser();
-
   return (
     <>
       <main className="h-screen w-screen bg-purple-500">
