45ddc6d3f264b2274ad513cb50c9f4e758105369
Tyler Hall <tacolegs2004@Tylers-MBP.lan>
2023-06-12 16:28:18 -0500

    post card is now a seperate component

---
+++ src/components/Post.tsx
@@ -0,0 +1,29 @@
+import { useUser } from "@clerk/nextjs";
+import React from "react";
+
+interface TTPost {
+  header: string;
+  body: string;
+  id: number;
+}
+
+interface TPost {
+  post: TTPost;
+}
+
+const Post: React.FC<TPost> = ({ post }) => {
+  const { user } = useUser();
+
+  return (
+    <>
+      <div key={post.id} className="m-5 flex flex-col space-y-4">
+        <h1 className="text-md font-bold">
+          {post.header} - {user?.username}
+        </h1>
+        <div className="">{post.body}</div>
+      </div>
+    </>
+  );
+};
+
+export default Post;
