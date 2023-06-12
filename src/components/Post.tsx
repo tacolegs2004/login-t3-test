86fb117153c5eabba18e6079471be1dd89632b49
Tyler Hall <tacolegs2004@Tylers-MBP.lan>
2023-06-12 17:29:04 -0500

    profile picture now displays on posts and now using <img />

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,4 @@
 import { useUser } from "@clerk/nextjs";
-import Image from "next/image";
 import React from "react";
 
 interface TTPost {
@@ -20,7 +19,7 @@ const Post: React.FC<TPost> = ({ post }) => {
       <div key={post.id} className="m-5 flex flex-col space-y-4">
         <h1 className="text-md font-bold">
           {post.header} - {user?.username}
-          <Image
+          <img
             src={user?.profileImageUrl}
             width={40}
             height={40}
