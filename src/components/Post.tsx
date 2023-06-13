da68a40bbe62cfc1754d3f627712881d62372721
Tyler Hall <tacolegs2004@Tylers-MBP.lan>
2023-06-13 11:36:36 -0500

    in process of moving to <Image />

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,5 @@
 import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 import React from "react";
 
 interface TTPost {
@@ -19,7 +20,7 @@ const Post: React.FC<TPost> = ({ post }) => {
       <div key={post.id} className="m-5 flex flex-col space-y-4">
         <h1 className="text-md font-bold">
           {post.header} - {user?.username}
-          <img
+          <Image
             src={user?.profileImageUrl}
             width={40}
             height={40}
