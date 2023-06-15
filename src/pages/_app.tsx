a3bc764f1e718a5b011c5841b02f4a557885f634
taco boi <tylerchall16@gmail.com>
2023-06-15 12:47:38 -0500

    fixed layout import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Layout from "~/components/Layout";
+import Layout from "../components/Layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
